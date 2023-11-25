d4076be930c71adb5353a2aaff59dc5ad1a633a0
Jerry-Khobby <jerrymardeburg@gmail.com>
2023-11-25 11:48:53 -1200

    done with the first register route left with testing

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,11 +2,50 @@ const bcrypt=require('bcrypt');
 
 const User =require("../models/schema");
 const dotenv=require("dotenv").config();
+const jwt =require("jsonwebtoken");
 
+const JWT_SECRET= process.env.JWT_SECRET
 
+const register =async(req,res)=>{
+    const {firstname,lastname,email,password}=req.body;
+    try{
+        const existingUser=await User.findOne({email});
+        if(existingUser){
+            console.log("This user already exists");
+            return res.status(409).json({error:"Email address is already in use"});
+        }
+        const newHashed =await bcrypt.hash(password,10);
+        // create a new following the password 
+        const newUser=new User({
+            firstname,
+            lastname,
+            email,
+            passwords: newHashed,
+        });
+        console.log("The new user created:",newUser);
+        await newUser.save();
 
-const register =(req,res)=>{
-    console.log("My name is Jeremiah Anku");
+        // now the user is created , let me add the jwt
+        const token = jwt.sign({ userId: newUser._id }, JWT_SECRET, { expiresIn: '1h' });
+        if (err) throw err;
+     // Respond with the token and any additional user information you want to include
+     res.cookie('token', token, {sameSite:'none', secure:true}).status(201).json({
+        id: newUser._id,
+      });
+     res.status(201).json({
+        message: "User registered successfully",
+        token,
+        user: {
+          userId: newUser._id,
+          email: newUser.email,
+          // Include other user information as needed
+        },
+      });
+    }catch(e){
+        if (err) throw err;
+        res.status(500).json('error');
+    
+    }
 }
 
 
--- models/schema.js
+++ models/schema.js
@@ -2,17 +2,17 @@ const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
-    first_name:{
+    firstname:{
         type:String,
         required:true,
+    },
+    lastname:{
+        type:String,
+        unique:true,
     },
     email:{
         type:String,
         required:true,
-        unique:true,
-    },
-    last_name:{
-        type:String,
         unique:true,
     },
     password:{
